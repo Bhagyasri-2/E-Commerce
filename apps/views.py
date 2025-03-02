e8933ef42122e035a513b48059feb2e803808ed7
Bhagyasri-2 <157514959+Bhagyasri-2@users.noreply.github.com>
2025-03-02 15:23:08 +0530

    Update views.py
    modified with all routes

--- apps/views.py
+++ apps/views.py
@@ -1,40 +1,50 @@
 from flask import Blueprint, render_template
-from app.models import db, Product  # Import db and Product from models
-import time
+from app.models import Men, Women, Kids, Accessories, Shoes  # Correct import
 
-views_bp = Blueprint('views', __name__, template_folder='templates')
+# Define Blueprint for views
+views_bp = Blueprint('views', __name__)
 
+# Index route to display products
 @views_bp.route('/')
 def index():
-    return render_template('index.html',now=int(time.time()))
+    # Query all products from different categories
+    products_men = Men.query.all()
+    products_women = Women.query.all()
+    products_kids = Kids.query.all()
+    products_accessories = Accessories.query.all()
+    products_shoes = Shoes.query.all()  # Correct Shoes model reference
 
-@views_bp.route('/women')
-def women():
-    
-    return render_template("women.html")
+    # Combine all products into one list
+    all_products = products_men + products_women + products_kids + products_accessories + products_shoes
+
+    # Render the homepage template and pass the products to the template
+    return render_template('index.html', products=all_products)
 
 @views_bp.route('/men')
 def men():
-    
-    return render_template("men.html")
+    products_men = Men.query.all()
+    return render_template('men.html', products=products_men, category="Men")
 
+@views_bp.route('/women')
+def women():
+    products_women = Women.query.all()
+    return render_template('women.html', products=products_women, category="Women")
 
 @views_bp.route('/kids')
 def kids():
-    
-    return render_template("kids.html")
-
+    products_kids = Kids.query.all()
+    return render_template('kids.html', products=products_kids, category="Kids")
 
 @views_bp.route('/accessories')
 def accessories():
-    
-    return render_template("accessories.html")
-
+    products_accessories = Accessories.query.all()
+    return render_template('accessories.html', products=products_accessories, category="Accessories")
 
 @views_bp.route('/shoes')
 def shoes():
-    
-    return render_template("kids.html")
+    products_shoes = Shoes.query.all()  # Correct query
+    return render_template('shoes.html', products=products_shoes, category="Shoes")
+
 
 
 
